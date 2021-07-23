3004ab98945b0a7679670d958e805404e21fbac9
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-23 15:58:58 -0400

    offline works

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,69 +1,80 @@
 const cacheName = 'v1'
-const cacheFiles = [
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
     '/',
     'index.js',
     'index.html',
     'style.css',
      'db.js'
-]
+];
 
+//install
 self.addEventListener('install', function(e) { 
-    console.log('Service worker is now working!!');
-
+    //pre cache budget data 
+    e.waitUntil( 
+        cache.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    );
 
     e.waitUntil(
         caches.open(cacheName)
-        .then(function (cache) { 
-            console.log('Caching files')
-            return cache.addAll(cacheFiles)
-        })
-    )
-})
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+     //lets browser know to activate this sevice worker immediately once it has finished installing
+     self.skipwaiting();
+        });
 
-self.addEventListener('acrive', function(e) { 
+self.addEventListener('activate', function(e) { 
     console.log('Service worker has been activated!!')
-
     e.waitUntil( 
         catches.keys()
-        .then(function (cacheNames) { 
-               return Promise.all(cacheNames.map(function (thisCacheName) { 
-                   if(thisCacheName !== cacheName) { 
-                       console.log('Files Removed from cache')
-                       return caches.delete(thisCacheName)
-                   }
-               }))
-        })
-    )
-})
+        .then(keyList => { 
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) { 
+                        console.log("Removing old cache data", key); 
+                        return caches.delete(key);
+                    }
+                })
+            );
+            })
+    );
+ self.clients.claim(); 
+        });
 
+
+
+//handel requests 
  self.addEventListener('fetch', function (e) { 
-     console.log('Service working is fetching', e.request.url)
+     if(e.request.url.includes('api')) {
+     console.log('[Service Worker] Fetch (data)', e.request.url);
+
+
      e.respondWith(
-         caches.match(e.request)
-         .then(function(response){ 
-             if (response) { 
-                 console.log(e.request.url)
-                 return response
-             }
-
-             const reqClone = e.request.clone()
-
-
-             fetch(reqClone)
-             .then(function (response) { 
-                 if(!response) { 
-                     console.log('fetch is not responding')
-                     return response 
+         caches.open.(DATA_CACHE_NAME).then(catche=> { 
+             return fetch(e.request).then(res => { 
+                 if(res.status === 200) { 
+                     cache.put(e.request.url, res.clone());
                  }
+                    return res;
+                }).catch(err => { 
+                    console.log(err);
+                    return cache.match(e.request);
+                })
+             })
 
-                 const respClone = response.clone.then(function(cache) { 
-                     cache.put(e.request, respClone ) 
-                     return response 
-                 })
+
+         );
+         return;
+
+            }
+            //sever files from cache
+        e.respondWith(
+         caches.open(CACHE_NAME).then(cache => { 
+             return cache.match(e.request).then(res => { 
+                 return res || fetch(e.request);
              })
-             .catch(function(error) { 
-                 console.log('error has been made', error); 
-             })
-         })
-     )
- })
+            })
+        );
+         });
+         
