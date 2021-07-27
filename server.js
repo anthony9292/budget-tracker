a3bf39a210894aebbf6f466cbec2013d367bc7f6
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-27 02:56:36 -0400

    fix2

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false,
 });
 
-const db = mongoose.connect
+const db = mongoose.connection;
 
 db.on('error', (error) => console.error(error));
 db.once('open', () => {
